cec916fe0b468914e13abb35d15e10f1a2c8e8c8
Dan Comand <dcomand@gmail.com>
2015-06-23 22:44:09 -0400

    Added form to add new task, and sorted them Created At:

--- simple-todos.js
+++ simple-todos.js
@@ -4,7 +4,24 @@ if (Meteor.isClient) {
 
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({});
+
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+
+      event.target.text.value = "";
+
+      return false;
     }
   });
 }
